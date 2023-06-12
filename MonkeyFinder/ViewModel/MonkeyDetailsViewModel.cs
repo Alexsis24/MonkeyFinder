564cced2d890e88fecd24421fa52bbc71c3cbb46
Alexsis24 <53233129+Alexsis24@users.noreply.github.com>
2023-06-12 15:28:03 +0300

    Added navigation to get to monkeys details

--- MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
+++ MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
@@ -9,4 +9,11 @@ public partial class MonkeyDetailsViewModel : BaseViewModel
     }
     [ObservableProperty]
     Monkey monkey;
+
+    //if you use modals (Shell.PresentationMode) use this to hava a backbutton
+    /*[RelayCommand]
+    async Task GoBackAsync()
+    {
+        await Shell.Current.GoToAsync("..");
+    }*/
 }
