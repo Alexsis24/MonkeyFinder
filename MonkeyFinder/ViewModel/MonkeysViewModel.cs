c3471c15cbfe50611414a21a5d39f58743f5debe
Alexsis24 <53233129+Alexsis24@users.noreply.github.com>
2023-06-15 12:53:31 +0300

    refresh works with android when ya pull down

--- MonkeyFinder/ViewModel/MonkeysViewModel.cs
+++ MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -6,10 +6,10 @@ namespace MonkeyFinder.ViewModel;
 
 public partial class MonkeysViewModel : BaseViewModel
 {
-    MonkeyService monkeyService;
+    
     public ObservableCollection<Monkey> Monkeys { get; } = new();
 
-
+    MonkeyService monkeyService;
     IConnectivity connectivity;
     IGeolocation geolocation;
     
@@ -23,57 +23,9 @@ public partial class MonkeysViewModel : BaseViewModel
         this.geolocation = geolocation;
     }
 
+    [ObservableProperty]
+    bool isRefreshing;
 
-    [RelayCommand]
-    async Task GetClosestMonkeyAsync()
-    {
-        if (IsBusy || Monkeys.Count == 0)
-            return;
-
-        try
-        {
-            // Get cached location, else get real location.
-            var location = await geolocation.GetLastKnownLocationAsync();
-            if(location == null)
-            {
-                location = await geolocation.GetLocationAsync(
-                    new GeolocationRequest
-                    {
-                        DesiredAccuracy = GeolocationAccuracy.Medium,
-                        Timeout = TimeSpan.FromSeconds(30),
-                    });
-            } 
-
-            // Find closest monkey to us
-            var first = Monkeys.OrderBy(m => location.CalculateDistance(m.Latitude, m.Longitude,DistanceUnits.Miles))
-                .FirstOrDefault();
-
-            if (first is null)
-                return;
-
-            await Shell.Current.DisplayAlert("Closest Monkey", $"{first.Name} in {first.Location}", "OK");
-
-        }
-        catch (Exception ex)
-        {
-            Debug.WriteLine(ex);
-            await Shell.Current.DisplayAlert("Error!", "Unable to get closest monkey.", "OK");
-        }
-    }
-
-
-    [RelayCommand]
-    async Task GoToDetailsAsync(Monkey monkey)
-    {
-        if (monkey is null)
-            return;
-
-        await Shell.Current.GoToAsync(nameof(DetailsPage), true,
-            new Dictionary<string, object>
-            {
-                {"Monkey", monkey}
-            });
-    }
 
     [RelayCommand]
 
@@ -107,6 +59,57 @@ public partial class MonkeysViewModel : BaseViewModel
         finally
         {
             IsBusy = false;
+            IsRefreshing = false;
+        }
+    }
+
+    [RelayCommand]
+    async Task GoToDetailsAsync(Monkey monkey)
+    {
+        if (monkey is null)
+            return;
+
+        await Shell.Current.GoToAsync(nameof(DetailsPage), true,
+            new Dictionary<string, object>
+            {
+                {"Monkey", monkey}
+            });
+    }
+
+    [RelayCommand]
+    async Task GetClosestMonkeyAsync()
+    {
+        if (IsBusy || Monkeys.Count == 0)
+            return;
+
+        try
+        {
+            // Get cached location, else get real location.
+            var location = await geolocation.GetLastKnownLocationAsync();
+            if (location is null)
+            {
+                location = await geolocation.GetLocationAsync(
+                    new GeolocationRequest
+                    {
+                        DesiredAccuracy = GeolocationAccuracy.Medium,
+                        Timeout = TimeSpan.FromSeconds(30),
+                    });
+            }
+
+            // Find closest monkey to us
+            var first = Monkeys.OrderBy(m => location.CalculateDistance(m.Latitude, m.Longitude, DistanceUnits.Miles))
+                .FirstOrDefault();
+
+            if (first is null)
+                return;
+
+            await Shell.Current.DisplayAlert("Closest Monkey", $"{first.Name} in {first.Location}", "OK");
+
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+            await Shell.Current.DisplayAlert("Error!", "Unable to get closest monkey.", "OK");
         }
     }
 }
